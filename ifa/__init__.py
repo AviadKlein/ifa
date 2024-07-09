d745eb138c7c6f12874458ed52f3d3f68ff0799f
Aviad Klein <aviad.klein@plarium.com>
2024-07-09 10:21:57 +0300

    version bump to 0.1.1

--- ifa/__init__.py
+++ ifa/__init__.py
@@ -4,4 +4,4 @@ from .api import *
 
 __all__ = ['api']
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ak_ifa",  
-    version="0.1.0",
+    version="0.1.1",
     author="Aviad Klein",
     author_email="aviad.klein@gmail.com",
     description="Incremental Feature Analysis Tool",
